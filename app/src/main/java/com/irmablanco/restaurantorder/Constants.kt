e843c301805323ce3e32d897bad3d8f30d795cb4
Irma <irblancofdz@gmail.com>
2018-06-02 20:01:46 +0200

    Add more resources and constant url for the api

---
+++ app/src/main/java/com/irmablanco/restaurantorder/Constants.kt
@@ -0,0 +1,3 @@
+package com.irmablanco.restaurantorder
+
+val CONSTANT_URL_JSON = "http://5b12d0bad50a5c0014ef1245.mockapi.io/dishes"
---
+++ app/src/main/java/com/irmablanco/restaurantorder/activity/TablePagerActivity.kt
@@ -0,0 +1,13 @@
+package com.irmablanco.restaurantorder.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.irmablanco.restaurantorder.R
+
+class TablePagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_pager)
+    }
+}
