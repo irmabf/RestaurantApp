92eb32f0eba6abc6262de52c7b5d2f46775d0aa7
Irma <irblancofdz@gmail.com>
2018-06-02 18:17:34 +0200

    Add setter and companion object to DishActivity

--- app/src/main/java/com/irmablanco/restaurantorder/activity/DishActivity.kt
+++ app/src/main/java/com/irmablanco/restaurantorder/activity/DishActivity.kt
@@ -8,28 +8,41 @@ import com.irmablanco.restaurantorder.model.Dish
 import kotlinx.android.synthetic.main.activity_dish.*
 
 class DishActivity: AppCompatActivity() {
-    val TAG = DishActivity::class.java.canonicalName
+    /**El tag aqui pertenecería a las instancias, seria un
+     * atributo de instancia o de objeto y eso
+     * no es  correcto, con lo que crearemos un companion object
+     *Lo que creemos en un companion object pertenece a la clase y no al objeto
+     *El companion object es un equivalente al static var de java,
+     * un metodo estatico tambien se meteria en el companion object*/
+    //val TAG = DishActivity::class.java.canonicalName
+    companion object {
+        val TAG = DishActivity::class.java.canonicalName
+    }
+
+    //Atributo para guardar el plato cuando nos lo pasen
+    //setter, para que cada vez que se mofica el atributo setter provocar
+    //una serie que modifica la interfaz
+    var dish: Dish? = null
+        //Cada vez que dish se modifique el setter asignara a la interfaz los nuevos valores
+        set(value) {
+            if (value != null) {
+                //Accedo por el id a las vistas del xml
+                dish_name.text = value.name
+                dish_image.setImageResource(value.image)
+                dish_description.text = value.description
+                dish_price.text = getString(R.string.price_format, value.price)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dish)
-
-        val dish = Dish(
+        //Este codigo me va a actualizar la interfaz
+        dish = Dish(
                 "Hamburguesa",
                 R.drawable.hamburguesa_patatas_1,
                 10f,
                 "Hamburguesa de pollo hecha  a la parrilla con patatas fritas")
 
-        setDish(dish)
-    }
-    /*Creo metodo setDish para pasar el plato que quiero crearme
-    y donde accedo directamente a las vistas de la interfaz*/
-
-    fun setDish(dish: Dish){
-        //Accedo por el id a las vistas del xml
-        dish_name.text = dish.name
-        dish_image.setImageResource(dish.image)
-        dish_description.text = dish.description
-        dish_price.text = getString(R.string.price_format, dish.price)
     }
 }
